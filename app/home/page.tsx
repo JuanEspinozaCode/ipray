71e7d216fbb02de44b2f9f2ead0ac8ef641d66ed
JuanEspinozaCode <146772106+JuanEspinozaCode@users.noreply.github.com>
2024-05-18 17:05:26 -0700

    Updated card

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 interface Props  {};
 
 const Home = (props: Props) => {
-  return  <div>Home</div>;
+  return  <div>iPray</div>;
   
 };
 
--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,30 @@
 import React from 'react';
 import Links from "./Routes/Routes"
+import Home from './home/page';
+import Card from './home/card';
 
 const mockData = [
   {
     id: 1,
-    prayer: "I want to be a better person",
+    prayer: "I want to grow in my walk with God and not be afraid of what's ahead.I want to grow in my walk with God and not be afraid of what's ahead.I want to grow in my walk with God and not be afraid of what's ahead.I want to grow in my walk with God and not be afraid of what's ahead.I want to grow in my walk with God and not be afraid of what's ahead.I want to grow in my walk with God and not be afraid of what's ahead.I want to grow in my walk with God and not be afraid of what's ahead.I want to grow in my walk with God and not be afraid of what's ahead.",
     username: "Alejandro Chang",
     date: "2021-07-02",
     likes: 0,
   },
-  {
-    id: 1,
-    prayer: "I want to be a better person",
-    username: "Juan Espinoza",
-    date: "2021-07-02",
-    likes: 0,
-  },
-  {
-    id: 1,
-    prayer: "I want to be a better person",
-    username: "Filipe",
-    date: "2021-07-02",
-    likes: 0,
-  },
+  // {
+  //   id: 1,
+  //   prayer: "I want to be a better person",
+  //   username: "Juan Espinoza",
+  //   date: "2021-07-02",
+  //   likes: 0,
+  // },
+  // {
+  //   id: 1,
+  //   prayer: "I want to be a better person",
+  //   username: "Filipe",
+  //   date: "2021-07-02",
+  //   likes: 0,
+  // }
 ];
  
 export default function App() {
@@ -33,6 +35,8 @@ export default function App() {
     <div>
       {/* {JSON.stringify(mockData)} */}
       <Links />
+      <Home />
+      <Card data = {mockData} />
     </div>
   );
 };

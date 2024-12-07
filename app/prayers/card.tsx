2bd445feb19a48243d356b12b341455163b699a5
Filipe Trevizan <108426894+Silentxlvp@users.noreply.github.com>
2024-12-07 13:41:30 -0800

    feat(i): card improvements

--- app/prayers/card.tsx
+++ app/prayers/card.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 
 interface DataItem {
   id: number;
@@ -12,33 +12,65 @@ interface DataItem {
 interface Props {
   data: DataItem[];
 }
-let imgs = [
+const imgs = [
   "https://images.vexels.com/media/users/3/155421/isolated/preview/a9fa9e88e0c885769014ee1023dbb98a-icono-de-cruz-cristiana-delgada.png",
   "https://www.onlygfx.com/wp-content/uploads/2018/08/6-grunge-cross-1.png",
 ];
+
 const Card: React.FC<Props> = ({ data }) => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const handlePrev = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex === 0 ? data.length - 1 : prevIndex - 1
+    );
+  };
+
+  const handleNext = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex === data.length - 1 ? 0 : prevIndex + 1
+    );
+  };
+
   return (
-    <div className="card-center">
+    <div>
       <div className="card-body">
-        {data.map((item, index) => (
-          <div key={item.id} className="card-un">
-            <div className="card">
-              <div className="card-text">
-                <p className="text-container">
-                  <h1 className="pray-header">Prayer</h1>
-                  {item.prayer}
-                </p>
-                <img
-                  src={imgs[0]}
-                  height={75}
-                  style={{ marginLeft: "5px" }}
-                  alt=""
-                />
-                <button>Like</button>
+        <div className="carousel">
+          {data.map((item, index) => (
+            <div
+              key={item.id}
+              className={`card-item ${
+                index === currentIndex ? "active" : "inactive"
+              }`}
+              style={{
+                display: index === currentIndex ? "block" : "none",
+              }}>
+              <div className="card">
+                <div className="card-text">
+                  <img
+                    src={imgs[0]}
+                    height={75}
+                    style={{ marginLeft: "5px" }}
+                    alt=""
+                  />
+                  <p className="username"> {item.username}</p>
+                  <p className="text-container">{item.prayer}</p>
+                  <br />
+                  
+                  <div className="buttons">
+                    <button className="handleButton" onClick={handlePrev}>
+                      ←
+                    </button>
+                    <button className="like-btn">Like</button>
+                    <button className="handleButton" onClick={handleNext}>
+                      →
+                    </button>
+                  </div>
+                </div>
               </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
     </div>
   );
--- app/prayers/page.tsx
+++ app/prayers/page.tsx
@@ -6,21 +6,21 @@ interface Props {}
 const mockData = [
   {
     id: 1,
-    prayer: "I want to be a better person",
+    prayer: "Lord of Peace, grant me tranquility in my heart and mind. Calm my fears and anxieties, and fill me with Your presence, so I may find rest in You. Help me to be a vessel of peace to others, spreading Your love wherever I go.",
     username: "Alejandro Chang",
     date: "2021-07-02",
     likes: 0,
   },
   {
     id: 1,
-    prayer: "I want to be a better person",
+    prayer: "Heavenly Father, I thank You for the many blessings You have bestowed upon me. I am grateful for the love, strength, and opportunities You provide, even in times of difficulty. May my heart always be filled with appreciation for Your endless grace and for the gift of each new day.",
     username: "Juan Espinoza",
     date: "2021-07-02",
     likes: 0,
   },
   {
     id: 1,
-    prayer: "I want to be a better person",
+    prayer: "Lord, guide my steps today as I seek clarity in the midst of uncertainty. Help me make decisions with wisdom and compassion, trusting that Your plan is always greater than my own. Surround me with Your light, so I may walk in faith and peace, knowing that You are with me in every moment.",
     username: "Filipe",
     date: "2021-07-02",
     likes: 0,

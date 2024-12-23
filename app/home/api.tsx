406b92fbf5c327bf85a6e3f6a495838d5c3d2d4f
Juan Espinoza <jesp9040@gmail.com>
2024-12-23 09:08:40 -0800

    API update

--- app/home/api.tsx
+++ app/home/api.tsx
@@ -6,18 +6,7 @@ export default function Api() {
   const [verse, setVerse] = useState<string>("");
 
   useEffect(() => {
-    const getRandomVerse = () => {
-      const books = ["genesis", "exodus", "leviticus", "numbers", "deuteronomy"];
-      const randomBook = books[Math.floor(Math.random() * books.length)];
-      const randomChapter = Math.floor(Math.random() * 50) + 1; // Assuming max 50 chapters
-      const randomVerse = Math.floor(Math.random() * 30) + 1; // Assuming max 30 verses per chapter
-
-      return `${randomBook}%20${randomChapter}:${randomVerse}`;
-    };
-
-    const verseUrl = `https://bible-api.com/${getRandomVerse()}`;
-
-    fetch(verseUrl)
+    fetch("https://bible-api.com/genesis%2011:1")
       .then(response => {
         if (!response.ok) {
           throw new Error("Network response was not ok");
